1364e9f04e83121482b1e6ef0445532e9fc56451
tobiaand <tobiaand@stud.ntnu.no>
2020-12-23 19:35:14 +0100

    TSP now working

--- src/travelling_salesman/SetOfPoints.js
+++ src/travelling_salesman/SetOfPoints.js
@@ -9,20 +9,21 @@ class SetOfPoints{
         this.draw = this.draw.bind(this);
         this.generatePoints = this.generatePoints.bind(this);
         this.generateEdges = this.generateEdges.bind(this);
+        this.dfsFlip = this.dfsFlip.bind(this);
 
         this.points = this.generatePoints(num_of_points, HEIGHT, WIDTH);
         this.edges = this.generateEdges(num_of_points);
         this.NUM_OF_POINTS = num_of_points;
+        this.local_minima_reached = false;
     }
 
     draw(c){
-        /*
         for (let i = 0; i < this.NUM_OF_POINTS; i++){
             let toIdx = this.edges[i];
             let toNode = this.points[toIdx];
             this.points[i].drawEdge(c, toNode);
         }
-        */
+        
         
         for (let p of this.points){
             p.draw(c);
@@ -30,12 +31,57 @@ class SetOfPoints{
     }
 
     climb(){ 
-        // basic implementation of hill climing algorithm, if we see a change that improves total distance, make the change
-        // produce all permuations of two selected points
-        // shuffle the list
-        // iterate though it
-        // for it pair, calculate the total distance if that change was made
-        // break when an improvment is made, otherwise flip a boolean that will skip the climb from here on out (local minima is reached)
+        // if we have reached the local minima we just skip the climbing altogether
+        if (!this.local_minima_reached){
+
+            let candidates = [] // all pairs of nodes, if a local improvement exists, it will be through swapping two nodes that will be in this list
+            for (let i = 0; i < this.NUM_OF_POINTS; i++){
+                for (let k = i+1; k < this.NUM_OF_POINTS; k++){
+                    candidates.push(
+                        {
+                            "A" : i,
+                            "C" : k
+                        }
+                    );    
+                }
+            }
+
+            // TODO: shuffle the candidates array here to produce somewhat more visually interesting results
+
+            for (let cand of candidates){
+                // we dont need to know the total length, we know we are making an improvment if the sum of the two new
+                // edges is smaller than the sum of the two old edges because all other edges stay the same
+                
+                // lets say that our old edges move from A to B, and from C to D;
+                let Aidx = cand["A"];
+                let Bidx = this.edges[Aidx];
+                let Cidx = cand["C"];
+                let Didx = this.edges[Cidx];
+                let A = this.points[Aidx];
+                let B = this.points[Bidx];
+                let C = this.points[Cidx];
+                let D = this.points[Didx];
+                
+                let oldDist = A.dist(B) + C.dist(D);
+                let newDist = A.dist(C) + D.dist(B); // if we had done a->d and b->c we would have created two closed loops instead of one large
+
+                if (newDist < oldDist){ // if we should climb in this direction
+                    this.edges[Aidx] = Cidx;
+                    //now the edges that used to go from B to C are facing the wrong way, lets flip them
+                    this.dfsFlip(Bidx, Didx, Cidx);
+                    return;
+                }
+            }
+            this.local_minima_reached = true;
+        }
+    }
+
+    //dfs for flipping the edges along a path
+    dfsFlip(node, prev, end){
+        if (node != end){
+            this.dfsFlip(this.edges[node], node, end);
+        }
+        this.edges[node] = prev;
     }
 
     generatePoints(n, HEIGHT, WIDTH){ // generate n points randomely distributed in the canvas, but dont place them to close to another
@@ -54,21 +100,10 @@ class SetOfPoints{
     }
 
     generateEdges(num_of_points){
-        let idxs = []
-        let edges = []
+        let edges = [];
+        // this is an ok way to produce random edges because there is no correlation between where the nodes are
         for (let i = 0; i < num_of_points; i++){
-            idxs.push(i);
-        }
-
-        // for each node, pick a random index among the nodes that have not been chosen, as long as this node is not itself
-        // create an edge to it.
-        for (let i = 0; i < num_of_points; i++){
-            let edgeTo = NaN;
-            do{
-                edgeTo = Math.floor(Math.random()*idxs.length);
-            } while(idxs[edgeTo] == i); // we dont want an edge from and to the same node
-            edges.push(idxs[edgeTo]);
-            idxs.splice(edgeTo, 1);
+            edges.push((i+1)%num_of_points); // add an edge to next index node
         }
 
         return edges;
