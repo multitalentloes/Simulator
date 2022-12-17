c794c9e2e4732f47396d2ae4551113d30c46daa2
multitalentloes <tobias4400@gmail.com>
2022-12-17 17:09:23 +0100

    WIP

--- src/waveequation/Grid.js
+++ src/waveequation/Grid.js
@@ -15,11 +15,20 @@ class Grid{
     }
 
     init(w, h){
+        let xwave = Math.floor(Math.random()*this.w);
+        let ywave = Math.floor(Math.random()*this.h);
+
         for (let i = 0; i < this.w; i++){
             for(let j = 0; j < this.h; j++){
+                //implementer en annen distanse funksjon som tar til betrakning mod!
+                let lx = Math.min(i, xwave);
+                let rx = Math.max(i, xwave);
+                let ly = Math.min(j, ywave);
+                let ry = Math.max(j, ywave);
                 this.grid[this.getidx(0, i, j, w, h)] = -Math.exp (
-                    -Math.pow(i-(w/4.0),2.0) / w
-                    -Math.pow(j-(h/4.0),2.0) / h);
+                    -Math.pow(Math.min(rx-lx, w-rx+lx),2.0) / w
+                    -Math.pow(Math.min(ry-ly, h-ry+ly),2.0) / h);
+
                 this.grid[this.getidx(1, i, j, w, h)] = this.grid[this.getidx(0, i, j, w, h)]
                 this.grid[this.getidx(2, i, j, w, h)] = 0.0
             }
